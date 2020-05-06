56486a51ee84d079bf792b4a62feaa537e1a62ea
Nimesh Subedi <nimesh.subedi1@gmail.com>
2020-05-06 11:38:57 -0600

    beasty

--- src/Players.js
+++ src/Players.js
@@ -2,5 +2,5 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   team7: require("./players/team7"),
-  beasty: require("./players/beasty"),
+  // beasty: require("./players/beasty"),
 };
