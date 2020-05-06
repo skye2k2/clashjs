68230d126ebba4ab562aa998bcf35e0fb4099679
Jakob Anderson <janderson@familysearch.org>
2020-05-06 11:34:20 -0600

    team 3 (jakobbot)

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,6 @@
 module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
+  jakob: require("./players/jakobbot"),
   // beasty: require("./players/beasty"),
 };
