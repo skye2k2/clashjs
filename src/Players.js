b10049f7e535e36aafac9efec9526942954ed286
Marshall Bean <cabobean@churchofjesuschrist.org>
2020-05-06 09:33:35 -0600

    Original setup

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,6 @@
 module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
+  fuzzypuppy: require("./players/fuzzyPuppy"),
   // beasty: require("./players/beasty"),
 };
