6d88ffb6db7b72d36417cac260848a884af2f977
Derrick Craven <dcravenus@gmail.com>
2020-05-06 11:18:30 -0600

    Update players

--- src/Players.js
+++ src/Players.js
@@ -2,4 +2,5 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   // beasty: require("./players/beasty"),
+  derricksBotty: require("./players/derricksBotty"),
 };
