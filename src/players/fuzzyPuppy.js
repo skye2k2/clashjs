d04ea184182a25d1ced745b822ef26315a4409d4
Marshall Bean <cabobean@churchofjesuschrist.org>
2020-05-06 11:40:43 -0600

    so far

--- src/players/fuzzyPuppy.js
+++ src/players/fuzzyPuppy.js
@@ -5,33 +5,106 @@ import {
   threatsFacingMe,
   canMoveForward,
   enemiesInRange,
+  isActionSafe
 } from "../lib/helpers";
+
+/*
+Improvements:
+how fast bullets move? 
+  Entire row instantly at players position
+Better determine when to shoot people? 
+  Can we track position of other players?
+    Enemies have direction, position, and ammo
+Does shooting take up a movement turn? [5]
+Can we turn the ship without moving it? [5]
+Is action safe? [1][x]
+*/
+
+
+/*
+  Enemies object
+
+  [
+   {
+      "style":103,
+      "position":[
+         0,
+         4
+      ],
+      "direction":"east",
+      "ammo":0,
+      "isAlive":true,
+      "id":"PCD0UV32",
+      "name":"random"
+   },
+   {
+      "style":25,
+      "position":[
+         2,
+         6
+      ],
+      "direction":"west",
+      "ammo":0,
+      "isAlive":true,
+      "id":"9PZZ1S52",
+      "name":"TestBot"
+   }
+]
+*/
 
 import debug from "debug";
 const log = debug("clashjs:bot:starterbot");
+var futureAction;
 
 export default {
   info: {
     name: "fuzzypuppy",
-    style: 38,
+    style: 110,
     team: 1,
   },
   ai: function (player, enemies, game) {
     log("Executing my AI function", player, enemies, game);
 
-    // Check if we are in immediate danger, if so try to move
-    if (threatsFacingMe(player, enemies).length > 0) {
-      log("In danger! Lets try to move");
-      if (canMoveForward(player, game)) {
-        return "move";
+    function checkForDanger(){
+      if(isActionSafe(player, futureAction, enemies, game)){
+        return futureAction;
+      }else{
+        futureAction = "";
       }
     }
+
+    // Check if we are in immediate danger, if so try to move
+    // check up down left right
+    if (threatsFacingMe(player, enemies).length > 0) {
+      log("In danger! Lets try to move");
+      // move where? calculate up down left right best position
+      if (canMoveForward(player, game)&&(isActionSafe(player, "move", enemies, game))) {
+        return "move";
+      }else{
+        //I cannot escape danger try to shoot instead
+        const targets = enemiesInRange(player, enemies);
+        if (player.ammo > 0 && targets.length > 0) {
+          log("Found someone to shoot", targets);
+          return "shoot";
+        }else{
+          //I cannot shoot anyone, time to pray
+          return makeRandomMove();
+        }
+      }
+    }
+
+    // calculate safe zone and hang out in it
+    
+
+    // Move to edges and corners to minimize vulnerable angles
+    
 
     // Not in danger, so lets see if we can shoot somebody
     const targets = enemiesInRange(player, enemies);
     if (player.ammo > 0 && targets.length > 0) {
       log("Found someone to shoot", targets);
-      return "shoot";
+      futureAction = "shoot";
+      checkForDanger();
     }
 
     // Not in danger, nobody to shoot, lets go collect more ammo
@@ -43,12 +116,13 @@ export default {
 
       log("Heading towards ammo", ammoDir);
       if (ammoDir === player.direction) {
-        return "move";
+        futureAction = "move";
+        checkForDanger();
       } else {
         return ammoDir;
       }
     }
-
+      
     // Nothing else to do ... lets just make a random move
     log("Bummer, found nothing interesting to do ... making random move");
     return makeRandomMove();
