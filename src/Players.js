fb47fe279a7fd14c212384f1d19fbcae12f38130
David Gurney <gurneyds@familysearch.org>
2020-05-06 11:27:04 -0600

    Dilbert player

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,6 @@
 module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
+  dilbert: require("./players/dilbert"),
   // beasty: require("./players/beasty"),
 };
