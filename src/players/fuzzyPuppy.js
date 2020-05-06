37681e6d06aa092ccf6527596d20935fd9a420ce
Marshall Bean <cabobean@churchofjesuschrist.org>
2020-05-06 11:53:49 -0600

    something

--- src/players/fuzzyPuppy.js
+++ src/players/fuzzyPuppy.js
@@ -52,6 +52,36 @@ Is action safe? [1][x]
 ]
 */
 
+/* Game 
+  {
+   "gridSize":7,
+   "ammoPosition":[
+
+   ],
+   "cargos":[
+
+   ],
+   "asteroids":[
+
+   ]
+}
+*/
+
+/* player
+  {
+   "style":20,
+   "position":[
+      6,
+      6
+   ],
+   "direction":"west",
+   "ammo":0,
+   "isAlive":true,
+   "id":"KINILKXV",
+   "name":"starterbot"
+}
+*/
+
 import debug from "debug";
 const log = debug("clashjs:bot:starterbot");
 var futureAction;
@@ -93,10 +123,67 @@ export default {
       }
     }
 
-    // calculate safe zone and hang out in it
+    function getPlayerNextTile(player){
+      if(player.direction === 'north'){
+        return [player.position[0]+1,player.position[1]]
+      }
+      if(player.direction === 'south'){
+        return [player.position[0]-1,player.position[1]]
+      }
+      if(player.direction === 'east'){
+        return [player.position[0],player.position[1]+1]
+      }
+      if(player.direction === 'west'){
+        return [player.position[0],player.position[1]-1]
+      }
+    }
+
+    function calculateClosestSafeZone(){
+        const myPosition = player.position;
+        const myDirection = player.direction;
+        const nextTile = getPlayerNextTile(player);
+        // loop through enemies, how in 1 turn how many enemies could be facing that tile?
+        let potentialDanger = 0;
+        for(let enemy of enemies){
+
+          if(enemy.position[0] < nextTile[0] && enemy.direction === 'south' ){
+            potentialDanger++
+          }
+          if(enemy.position[0] > nextTile[0] && enemy.direction === 'north' ){
+            potentialDanger++
+          }
+          if(enemy.position[1] < nextTile[1] && enemy.direction === 'west'){
+            potentialDanger++
+          }
+          if(enemy.position[1] > nextTile[1] && enemy.direction === 'east'){
+            potentialDanger++
+          }
+        }
+        if(potentialDanger > 0){
+          return 'move'
+        }
+    }   
+    calculateClosestSafeZone()
+/*
+    // Move to edges and corners to minimize vulnerable angles
+    var farEdge = (game.gridSize)-1; //integer
+    var myPosition = player.position; //[x,y]
+    var closestEdge; //north, east, south, west
+    var fourDirections =[myPosition[0], myPosition[1]-farEdge, myPosition[0]-farEdge, myPosition[1]];
+    var direction = 0; //North, East, South, West
+    for (var i = 1; i < fourDirections.length; i++) {
+      if (fourDirections[i] < fourDirections[lowest]) lowest = i;
+    }
+    switch(direction){
+      case 0: closestEdge = "north";
+      case 1: closestEdge = "east";
+      case 2: clos"south"
+      case 3: "west"
+    }
+    var facing = player.facing;
+*/
     
 
-    // Move to edges and corners to minimize vulnerable angles
     
 
     // Not in danger, so lets see if we can shoot somebody
