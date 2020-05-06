77c6f75b736c7220b3357bf01d562639b17a3739
Trevor Adams <nytadams@familysearch.org>
2020-05-06 11:30:58 -0600

    adding rumba bot

--- src/Players.js
+++ src/Players.js
@@ -2,4 +2,5 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   // beasty: require("./players/beasty"),
+  rumba: require("./players/rumba"),
 };
