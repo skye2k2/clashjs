692c7bfda4e8db253c0f20b7eed9586d71be704f
Spencer Memmott <Spenstar@users.noreply.github.com>
2020-05-06 11:34:04 -0600

    ninjaPuppies - Team 5

--- src/Players.js
+++ src/Players.js
@@ -2,4 +2,5 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   // beasty: require("./players/beasty"),
+  ninjaPuppies: require("./players/ninja-puppies"),
 };
