f4b4c17dbaf2dd929ad6f73d1f6c958ef9756c84
bonniesoderborg <Bonnie.Soderborg@familysearch.org>
2020-05-06 11:15:02 -0600

    next

--- src/Players.js
+++ src/Players.js
@@ -2,5 +2,5 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   team25: require("./players/team25"),
-  // beasty: require("./players/beasty"),
+  beasty: require("./players/beasty"),
 };
