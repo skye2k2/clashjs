6fdab69ed80730e2734ca173a94f2725daa7ce13
Natasha Collins <natasha.collins@familysearch.org>
2020-05-06 11:30:09 -0600

    Team 18 Is the Best!

--- src/Players.js
+++ src/Players.js
@@ -2,4 +2,5 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   // beasty: require("./players/beasty"),
+  team18: require("./players/team18"),
 };
