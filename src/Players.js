0771c9c248e5e7b9af56b4434052b6b4343db0f6
Nathan Huff <huffn@churchofjesuschrist.org>
2020-05-06 11:33:11 -0600

    Adding Intimidating Bot

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,6 @@
 module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
+  somethingIntimidating: require("./players/somethingIntimidating"),
   // beasty: require("./players/beasty"),
 };
