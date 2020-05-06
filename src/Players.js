28cbb701f34cf85a39126abd8f858cfd610b155e
Brennon Schow <brennon.schow@familysearch.org>
2020-05-06 10:28:38 -0600

    initial bot config

--- src/Players.js
+++ src/Players.js
@@ -2,4 +2,5 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   // beasty: require("./players/beasty"),
+  alwaysAngry: require('./players/alwaysAngry')
 };
---
+++ src/players/alwaysAngry.js
@@ -0,0 +1,64 @@
+import {
+  makeRandomMove,
+  sameColumn,
+  sameRow,
+  calculateDistance,
+  calculateHeading,
+  calculateNewPosition,
+  isTargetVisible,
+  enemiesInRange,
+  threats,
+  threatsFacingMe,
+  canMoveForward,
+  findClosestAmmo,
+  isActionSafe,
+  oppositeDirection,
+} from "../lib/helpers";
+
+import debug from "debug";
+const log = debug("clashjs:bot:starterbot");
+
+export default {
+  info: {
+    name: "AlwaysAngry",
+    style: 31,
+    team: 8,
+  },
+  ai: function (player, enemies, game) {
+    log("Executing my AI function", player, enemies, game);
+
+    // Check if we are in immediate danger, if so try to move
+    if (th1reatsFacingMe(player, enemies).length > 0) {
+      log("In danger! Lets try to move");
+      if (canMoveForward(player, game)) {
+        return "move";
+      }
+    }
+
+    // Not in danger, so lets see if we can shoot somebody
+    const targets = enemiesInRange(player, enemies);
+    if (player.ammo > 0 && targets.length > 0) {
+      log("Found someone to shoot", targets);
+      return "shoot";
+    }
+
+    // Not in danger, nobody to shoot, lets go collect more ammo
+    const closestAmmo = findClosestAmmo(player, game);
+
+    if (closestAmmo) {
+      log("Found some ammo", closestAmmo);
+      const ammoDir = calculateHeading(player.position, closestAmmo);
+
+      log("Heading towards ammo", ammoDir);
+      if (ammoDir === player.direction) {
+        return "move";
+      } else {
+        return ammoDir;
+      }
+    }
+
+    // Nothing else to do ... lets just make a random move
+    log("Bummer, found nothing interesting to do ... making random move");
+    return makeRandomMove();
+  },
+};
