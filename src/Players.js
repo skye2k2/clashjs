83174cdf477eccb0e9e02a9d2f6e47c1bf29c230
Andrew Wigginton <anscwi@churchofjesuschrist.org>
2020-05-06 11:29:17 -0600

    shootermcgavin

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,6 @@
 module.exports = {
-  starterbot: require("./players/starterbot"),
-  random: require("./players/randombot"),
+  starterbot: require('./players/starterbot'),
+  random: require('./players/randombot'),
   // beasty: require("./players/beasty"),
-};
+  shootermcgavin: require('./players/shootermcgavin'),
+}
